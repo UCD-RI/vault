f044bc9f85c417a0b44fe345e9ea82f7bbfd4b65
vishalnayak <vishalnayakv@gmail.com>
2019-01-10 17:11:28 -0500

    Fix the request passthrough problem

--- command/agent_ping.go
+++ command/agent_ping.go
@@ -62,14 +62,27 @@ func (c *AgentPingCommand) Run(args []string) int {
 		return 2
 	}
 
-	secret, err := client.Logical().Read("auth/approle/role/approle")
+	secret, err := client.Logical().Read("auth/approle/role/approle/role-id")
 	if err != nil {
-		c.UI.Error(fmt.Sprintf("Error reading auth/approle/role/approle: %v", err))
+		c.UI.Error(fmt.Sprintf("Error reading role id: %v", err))
 		return 2
 	}
+	roleID := secret.Data["role_id"].(string)
 
-	if c.flagField != "" {
-		return PrintRawField(c.UI, secret, c.flagField)
+	secret, err = client.Logical().Write("auth/approle/role/approle/secret-id", nil)
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error reading secret id: %v", err))
+		return 2
+	}
+	secretID := secret.Data["secret_id"].(string)
+
+	secret, err = client.Logical().Write("auth/approle/login", map[string]interface{}{
+		"role_id":   roleID,
+		"secret_id": secretID,
+	})
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error performing approle login: %v", err))
+		return 2
 	}
 
 	return OutputSecret(c.UI, secret)

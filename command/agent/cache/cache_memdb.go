a3d2220f530d3fa3b69ac7ad5ea1b946041a9cea
vishalnayak <vishalnayakv@gmail.com>
2019-01-15 08:46:37 -0500

    File renaming

--- command/agent/cache/cache_memdb.go
+++ command/agent/cache/cache_memdb.go
@@ -1,9 +1,12 @@
 package cache
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha256"
 	"errors"
 	"fmt"
+	"net/http"
 
 	hclog "github.com/hashicorp/go-hclog"
 	memdb "github.com/hashicorp/go-memdb"
@@ -211,4 +214,19 @@ func (c *Cache) DeleteByPrefix(keyType, prefix string) error {
 	}
 
 	return nil
+}
+
+// ComputeCacheKey results in a value that uniquely identifies a request
+// received by the agent. It does so by SHA256 hashing the marshalled JSON
+// which contains the request path, query parameters and body parameters.
+func ComputeCacheKey(req *http.Request) (string, error) {
+	var b bytes.Buffer
+
+	// Serialze the request
+	if err := req.Write(&b); err != nil {
+		return "", fmt.Errorf("unable to serialize request: %v", err)
+	}
+
+	sum := sha256.Sum256(b.Bytes())
+	return string(sum[:]), nil
 }

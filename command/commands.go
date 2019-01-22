2894889f4b804b22a5623d1523ca8ea55cad4be6
vishalnayak <vishalnayakv@gmail.com>
2019-01-22 14:06:08 -0500

    Remove POC code

--- command/commands.go
+++ command/commands.go
@@ -207,11 +207,6 @@ func initCommands(ui, serverCmdUi cli.Ui, runOpts *RunOptions) {
 				ShutdownCh: MakeShutdownCh(),
 			}, nil
 		},
-		"agent ping": func() (cli.Command, error) {
-			return &AgentPingCommand{
-				BaseCommand: getBaseCommand(),
-			}, nil
-		},
 		"audit": func() (cli.Command, error) {
 			return &AuditCommand{
 				BaseCommand: getBaseCommand(),

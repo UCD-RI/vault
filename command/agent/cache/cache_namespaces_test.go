0cbed466c3ac5bf648156a8fa09a61b32648bdc0
Calvin Leung Huang <cleung2010@gmail.com>
2019-02-06 10:19:13 -0800

    cache: add testSendNamespaces; pass req headers to api proxy client

--- command/agent/cache/cache_namespaces_test.go
+++ command/agent/cache/cache_namespaces_test.go
@@ -1,20 +1,25 @@
 package cache
 
 import (
+	"context"
+	"fmt"
+	"net"
+	"os"
 	"testing"
 
 	"github.com/go-test/deep"
 	hclog "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/helper/logging"
+	vaulthttp "github.com/hashicorp/vault/http"
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/vault"
 )
 
 func TestCache_Namespaces(t *testing.T) {
-	t.Parallel()
-	t.Run("send", testSendNamespaces)
-	t.Run("handle_cacheclear", testHandleCacheClearNamespaces)
-	t.Run("eviction_on_revocation", testEvictionOnRevocationNamespaces)
+	testSendNamespaces(t)
+	testHandleCacheClearNamespaces(t)
+	testEvictionOnRevocationNamespaces(t)
 }
 
 func testSendNamespaces(t *testing.T) {
@@ -27,29 +32,74 @@ func testSendNamespaces(t *testing.T) {
 		},
 	}
 
-	cleanup, clusterClient, testClient := setupClusterAndAgent(t, coreConfig)
-	defer cleanup()
+	cluster := vault.NewTestCluster(t, coreConfig, &vault.TestClusterOptions{
+		HandlerFunc: vaulthttp.Handler,
+	})
+	cluster.Start()
+	defer cluster.Cleanup()
+
+	cores := cluster.Cores
+	vault.TestWaitActive(t, cores[0].Core)
+	client := cores[0].Client
 
 	// Create a namespace
-	_, err := clusterClient.Logical().Write("sys/namespaces/ns1", nil)
+	_, err := client.Logical().Write("sys/namespaces/ns1", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// Mount the leased KV into ns1
-	clusterClient.SetNamespace("ns1/")
-	err = clusterClient.Sys().Mount("kv", &api.MountInput{
+	client.SetNamespace("ns1/")
+	err = client.Sys().Mount("kv", &api.MountInput{
 		Type: "kv",
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	clusterClient.SetNamespace("")
+	client.SetNamespace("")
+
+	// Set up env vars for agent consumption
+	defer os.Setenv(api.EnvVaultAddress, os.Getenv(api.EnvVaultAddress))
+	os.Setenv(api.EnvVaultAddress, client.Address())
+
+	defer os.Setenv(api.EnvVaultCACert, os.Getenv(api.EnvVaultCACert))
+	os.Setenv(api.EnvVaultCACert, fmt.Sprintf("%s/ca_cert.pem", cluster.TempDir))
+
+	cacheLogger := logging.NewVaultLogger(hclog.Trace)
+	ctx := context.Background()
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+
+	// Start listening to requests
+	err = Run(ctx, &Config{
+		Token:            client.Token(),
+		UseAutoAuthToken: false,
+		Listeners:        []net.Listener{listener},
+		Logger:           cacheLogger.Named("cache"),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Clone a client to query from the agent's listener address
+	testClient, err := client.Clone()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := testClient.SetAddress("http://" + listener.Addr().String()); err != nil {
+		t.Fatal(err)
+	}
+
+	testClient.SetToken(cluster.RootToken)
 
 	// Try request using full path
 	{
 		// Write some random value
-		_, err = clusterClient.Logical().Write("/ns1/kv/foo", map[string]interface{}{
+		_, err = client.Logical().Write("/ns1/kv/foo", map[string]interface{}{
 			"value": "test",
 			"ttl":   "1h",
 		})
@@ -75,7 +125,7 @@ func testSendNamespaces(t *testing.T) {
 	// Try request using the namespace header
 	{
 		// Write some random value
-		_, err = clusterClient.Logical().Write("/ns1/kv/bar", map[string]interface{}{
+		_, err = client.Logical().Write("/ns1/kv/bar", map[string]interface{}{
 			"value": "test",
 			"ttl":   "1h",
 		})
@@ -104,7 +154,7 @@ func testSendNamespaces(t *testing.T) {
 	// full path), they should not be the same cache entry (i.e. should produce
 	// different lease ID's).
 	{
-		_, err := clusterClient.Logical().Write("/ns1/kv/baz", map[string]interface{}{
+		_, err := client.Logical().Write("/ns1/kv/baz", map[string]interface{}{
 			"value": "test",
 			"ttl":   "1h",
 		})
